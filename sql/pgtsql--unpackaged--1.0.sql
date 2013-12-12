c9b706ee47f827a9f9daa1daca349b288aa972aa
Jim Mlodgenski <jimm@openscg.com>
2013-12-12 14:46:35 -0500

    Initial Commit

---
+++ sql/pgtsql--unpackaged--1.0.sql
@@ -0,0 +1,9 @@
+/* 
+ * Copyright (c) 2011-2013, Open Source Consulting Group, Inc.  All rights reserved.
+ */
+
+ALTER EXTENSION pltsql ADD PROCEDURAL LANGUAGE pltsql;
+-- ALTER ADD LANGUAGE doesn't pick up the support functions, so we have to.
+ALTER EXTENSION pltsql ADD FUNCTION pltsql_call_handler();
+ALTER EXTENSION pltsql ADD FUNCTION pltsql_inline_handler(internal);
+ALTER EXTENSION pltsql ADD FUNCTION pltsql_validator(oid);
